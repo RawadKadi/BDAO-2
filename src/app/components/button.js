f49fba1e975b57af5f08555fcaf357778512c375
RawadKadi <rawadkady@gmail.com>
2023-06-07 21:54:40 +0300

    leave a note button

--- src/app/components/button.js
+++ src/app/components/button.js
@@ -1,12 +1,12 @@
 import "./buttons.scss";
-import button from "./button-3d.png"
+import button from "./button-3d.svg"
 import Image from "next/image";
 
 export default function Button() {
     return (
         <>
     <div id="contact-button" >
-                <p>Leave a note </p>
+                <p style={{color:"white"}}>Leave a note </p>
                 <Image src={button} alt="Button Image" width={300} height={200} />
 
             </div>
