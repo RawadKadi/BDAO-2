f85ede499c44e6fc0de1eac59283702cdf25ba42
loque18 <madridx2010@gmail.com>
2023-06-09 13:18:09 -0500

    feat: fixed built issues3

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,12 +1,8 @@
-import Button from "@/components/3d-button";
-
 export default function TestPage() {
     return (
         <section className="h-[100vh] border-t border-[blue] container mx-auto">
             <h1 className="text-white text-center">Hey this is a title</h1>
-            <div className="flex bg-[#45459e]">
-                <Button>asd</Button>
-            </div>
+            <div className="flex bg-[#45459e]"></div>
         </section>
     );
 }
